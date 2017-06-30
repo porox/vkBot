18899678d2817afd78cbef115eddecdf2820f643
ydombrovsky <ydombrovsky95@gmail.com>
2017-06-30 21:48:29 +0400

    off crons;

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -1,5 +1,5 @@
 <?php
 exec("git checkout master --force ");
 exec("git pull origin master");
-require_once "stopCrons.php";
-require_once "startCrons.php";
+//require_once "stopCrons.php";
+//require_once "startCrons.php";
