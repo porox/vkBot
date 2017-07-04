574ea32b6135ff4c25a511d1b12cefa78e36d193
ydombrovsky <ydombrovsky95@gmail.com>
2017-07-05 00:14:26 +0400

    test conect

--- VkReposter.php
+++ VkReposter.php
@@ -1,23 +1,6 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-use \Posts\Posts;
+$res =\vkBot\App::get()->getPDOConnection()->query("SELECT * FROM postsNeedSend")->fetchAll();
 
-$vk = \vkBot\App::get()->getVkInstanse();
-
-sleep(1);
-$result = $vk->request('newsfeed.search', [
-	'q'        => 'репост лайк конкурс like розыгрыш приз',
-	'extended' => '',
-	'count'    => 15,
-])->getResponse();
-
-$test = new Posts($result);
-\vkBot\App::get()->getPDOConnection()->beginTransaction();
-try
-{
-	\vkBot\App::get()->getPDOConnection()->commit();
-} catch (Exception $e)
-{
-	\vkBot\App::get()->getPDOConnection()->rollBack();
-}
+var_dump($res);
